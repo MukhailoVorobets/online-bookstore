27dc7bc13edde77aabc11773420e89797132954b
Mikhailo <88093373+MukhailoVorobets@users.noreply.github.com>
2024-12-11 22:46:05 +0200

    add BookRepositoryImpl and BookServiceImpl

---
+++ src/main/java/com/example/onlinebookstore/service/BookServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.onlinebookstore.service;
+
+import com.example.onlinebookstore.entity.Book;
+import com.example.onlinebookstore.repository.BookRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
